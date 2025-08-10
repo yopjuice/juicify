ec3f84c2b5f1141fa9e704a5f00b5460e0209795
tony <tony@gmail.com>
2025-08-10 17:22:05 +0300

    new one

--- src/user/decorators/user.decorator.ts
+++ src/user/decorators/user.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { User } from "@prisma/client";
+import type { User } from "@prisma/client";
 
 export const CurrentUser = createParamDecorator(
     (data: keyof User, ctx: ExecutionContext) => {
